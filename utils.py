bf8bc58cab8ef17a18713c220333c0c75b6b7144
Tristan D'audi <tristan.daudibert@gmail.com>
2022-07-04 17:51:27 +0200

    utils.py: add get_timezone to have a clean usage of published_since parameter

--- utils.py
+++ utils.py
@@ -1,16 +1,15 @@
 from logger import Logger
 from pprint import pprint
+import pytz
 
 logger = Logger(2).get_logger()
 
 class Utils:
+    def get_timezone():
+        tz = 'utc+2'
+        try:
+            timezone = pytz.timezone(tz)
+        except Exception:
+            timezone = pytz.utc
 
-    def add_to_latest_post(self, new_post, index) -> None:           
-
-        no_post_registered = len(self.latest_feed_posted) == 0
-        current_feed_is_registered = 0 <= index < len(self.latest_feed_posted)
-
-        if no_post_registered or not current_feed_is_registered:
-            self.latest_feed_posted.append(new_post.title)
-        else:
-            self.latest_feed_posted[index] = new_post.title
+        return timezone
