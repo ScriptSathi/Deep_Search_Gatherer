0906594c32a0ce98382c6b4765ed9d0ac34e066f
Tristan D'audi <tristan.daudibert@gmail.com>
2022-07-04 14:59:10 +0200

    main.py: setup a test stage with a single feed parsed

--- main.py
+++ main.py
@@ -1,45 +1,54 @@
-import feedparser, asyncio
-from event import EventHandler
+import asyncio
 
 from logger import Logger
 from feeds import Feeds
+from RSSManager import RSSManager
+from event import EventHandler
 
 logger = Logger(2).get_logger()
 
 class RSSBot:
+
+    handler = EventHandler()
+
     def __init__(self) -> None:
-        self.handle = EventHandler()
-        self.feeds = Feeds(self.handle)
+        self.feeds = Feeds(self.handler)
 
-        self.feeds.add_events()
+    def crawl_feeds(self):
 
-    def trigger_post_event(self, all_posts, index):
-        no_need_to_post, post_until_latest, add_all_post_until_time = (0, 1, 2)
-        case = self.feeds.get_status_of_feed(all_posts, index)
+        # for index_of_current_feed, feed_config in enumerate(self.feeds.list_data):
 
-        if no_need_to_post == case:
-            self.handle.on('nothing_to_post', 'There is no new post on this feed')
-        elif post_until_latest == case:
-            self.handle.on('post_until_latest', all_posts, index)
-        if add_all_post_until_time == case:
-            self.handle.on('append_new_feed', all_posts, index)
+        index_of_current_feed = 0
+        feed_url = 'https://www.cert.ssi.gouv.fr/avis/feed/'
+        latest_post_feed = self.feeds.latest_feed_posted[index_of_current_feed]
 
-    async def poll_feed(self):
-        for index, feed in enumerate(self.feeds.list):
+        rss_manager = RSSManager(feed_url, index_of_current_feed, latest_post_feed, self.handler)
 
-            all_posts = feedparser.parse(feed['url']).entries
+        feed_status_code = self.feeds.get_status_of_feed(rss_manager.all_posts, index_of_current_feed)
 
-            self.trigger_post_event(all_posts, index)
+        latest_post_name = rss_manager.update_and_return_latest_rss(feed_status_code)
+        logger.info(latest_post_name)
+        # self.feeds.update_feed_posts(latest_post_name, index_of_current_feed)
 
-            for new_post in reversed(all_posts):
-                pass
 
-    async def call_data(self):
+
+
+
+        #         # for index_of_current_feed, feed_config in enumerate(self.feeds.list_data):
+
+        #         rss_manager = RSSManager(feed_config['url'], index_of_current_feed, self.handler)
+
+        #         feed_status_code = self.feeds.get_status_of_feed(rss_manager.all_posts, index_of_current_feed)
+
+        #         latest_post_name = rss_manager.update_and_return_latest_rss(feed_status_code)
+        #         self.feeds.update_feed_posts(latest_post_name, index_of_current_feed)
+
+    async def run(self):
         while True:
-            await self.poll_feed()
+            self.crawl_feeds()
             await asyncio.sleep(5)
 
 if __name__ == "__main__":
     asyncio.run(
-        RSSBot().call_data()
+        RSSBot().run()
     )
