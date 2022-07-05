7c0f76cd7c6b6569ad50e24c261b054b0083a372
Tristan D'audi <tristan.daudibert@gmail.com>
2022-07-05 16:11:25 +0200

    main.py: add discord.py client

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
-import asyncio
+import asyncio, discord
+from constants import Constants
 from threading import Thread
 
 from logger import Logger
 from RSSManager import RSSManager
 from event import EventHandler
 from parser import Parser
+from message import Message
+
+from rss_bot import RSSBot
 
 logger = Logger(2).get_logger()
 
-class RSSBot:
+class Client(discord.Client):
 
-    handler = EventHandler()
-    parser = Parser()
+    def __init__(self, **discord_params) -> None:
+        super().__init__(**discord_params)
+        parser = Parser()
+        config = parser.get_config()
 
-    def __init__(self) -> None:
-        self.config = self.parser.get_config()
+        self.config = config
 
-    def run(self):
-        threads = []
-        for feed_config in self.config['feeds']:
-            rss_manager = RSSManager(feed_config, self.handler, self.parser)
-            current_thread = Thread(target=rss_manager.run)
-            threads.append(current_thread)
-            current_thread.start()
+    async def on_ready(self) -> None:
+        logger.info(f'Logged in as {self.user} (ID: {self.user.id})')
+        logger.info('------')
+        await self.loop.create_task(self.my_background_task())
+
+    async def my_background_task(self):
+        await self.wait_until_ready()
+        while not self.is_closed():
+            await RSSBot(self, self.config).run()
 
 if __name__ == "__main__":
-    RSSBot().run()
+    parser = Parser()
+    token = parser.get_token()
 
+    Client(
+        chunk_guilds_at_startup=False,
+        member_cache_flags=discord.MemberCacheFlags.none(),
+        max_messages=None,).run(token
+    )
