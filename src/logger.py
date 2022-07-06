9aa50308cbedcce22e72e641466f7c42e8ad125d
Tristan D'audi <tristan.daudibert@gmail.com>
2022-07-06 12:18:23 +0200

    logger.py: update logger usage

--- src/logger.py
+++ src/logger.py
@@ -1,8 +1,9 @@
 import logging
 
 class Logger:
-    def get_logger(self) -> logging:
-        logging.basicConfig(format='%(asctime)s [%(funcName)s:%(lineno)s] - %(levelname)s: %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=self.log_level)
+    def get_logger() -> logging:
+        log_level = logging.INFO
+        logging.basicConfig(format='%(asctime)s [%(funcName)s:%(lineno)s] - %(levelname)s: %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=log_level)
         logger = logging.getLogger(__name__)
-        logger.setLevel(self.log_level)
+        logger.setLevel(log_level)
         return logging
