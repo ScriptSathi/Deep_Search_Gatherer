23b8cfa1ed6a42bf461108474c431a949410206f
Tristan D'audi <tristan.daudibert@gmail.com>
2022-07-06 13:21:47 +0200

    message.py: change _build_ message() for including youtube

--- src/message.py
+++ src/message.py
@@ -1,8 +1,42 @@
+import re
+
+from html2text import HTML2Text
+
 from src.logger import Logger
 
 logger = Logger.get_logger()
 
 class Message:
+
+    def parse_html(html_content): 
+        htmlfixer = HTML2Text()
+        logger.info(htmlfixer)
+        htmlfixer.ignore_links = True
+        htmlfixer.ignore_images = True
+        htmlfixer.ignore_emphasis = False
+        htmlfixer.body_width = 1000
+        htmlfixer.unicode_snob = True
+        htmlfixer.ul_item_mark = "-" 
+        markdownfield = htmlfixer.handle(html_content)
+        return re.sub("<[^<]+?>", "", markdownfield)
+
+    def render_author(news):
+        if 'author  s' or 'author' in news:
+            if 'authors' in news:
+                str_authors = 'Author: '
+                if len(news['authors']) == 1:
+                    str_authors += (news['authors'][0]).name
+                else: 
+                    for author in news['authors']:
+                        str_authors += author.name + ', '
+                return str_authors
+            elif 'author' in news: 
+                return news['author'].name
+        return ''
+
+    def is_youtube_feed(news):
+        return 'yt_videoid' in news
+
     def __init__(self, client, channels, feed_config) -> None:
         self.client = client
         self.channels = channels
@@ -21,14 +55,18 @@ class Message:
         logger.info(f'{self.feed_config["name"]} - Publishing on channel "{channel.name}" - "{news.title}"')
 
     def _build_message(self, news):
+
+        auth = Message.render_author(news)
         message = ''
 
-        title = f'**{news.title}**'
-        published = news.published
+        title = f'***{news.title}***'
+        author = f'*{auth}*' if auth != '' else ''
+        published = news.published if not Message.is_youtube_feed(news) else ''
         link = news.link
-        summary = news.summary
+        summary = Message.parse_html(news.summary) if not Message.is_youtube_feed(news) else ''
 
-        for field in (title, published, link, summary):
-            message += field + '\n'
+        for field in (title, author, published, link, summary):
+            if field != '' :
+                message += field + '\n'                
 
         return message
