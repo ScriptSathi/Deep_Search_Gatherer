350fc834f6ade8a68b3634242bc925e92e3bb0ad
Tristan D'audi <tristan.daudibert@gmail.com>
2022-07-11 01:05:17 +0200

    src/constants.py: add path for latest_post_feed file with default set to /home/rssbot

--- src/constants.py
+++ src/constants.py
@@ -2,6 +2,8 @@ import os
 
 class Constants: 
     json_conf_path_dir = os.path.join('/config')
+    home_dir = os.environ['HOME'] if os.environ['HOME'] else '/home/rssbot'
+    json_latest_post_data_path = os.path.join(home_dir, 'json_saved_feeds_data')
     default_config = {
             "token": "",
             "feeds": [],
