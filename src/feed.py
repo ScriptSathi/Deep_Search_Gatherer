3af4c36fccba04f97bd7596ec47c40a7af99ea6c
Tristan D'audi <tristan.daudibert@gmail.com>
2022-08-13 16:58:34 +0200

    src/feed.py: Refacto && fix bug of unposted news when published_since==0

--- src/feed.py
+++ src/feed.py
@@ -16,93 +16,35 @@ class Feed:
         self.latest_post = latest_post
         self.feed_config = feed_config
         self.channels = chan
-
-        self.all_posts = self._get_xml_feed(feed_config['url'], generator_exist)
+        self.generator_exist = generator_exist
+        
+        all_posts = self._get_xml_feed(self.feed_config['url'])
+        self.news_to_publish = self._get_unsended_news(all_posts)
 
     def run(self, client) -> None:
         self.message = Message(client, self.channels, self.feed_config)
-        while True:
-            case = self._get_status_of_feed()
-            self._send_message_if_needed(case)
-            self._close_thread_after_usage()
+        self._send_message_if_needed()
+        self._close_thread()
 
-    def _register_latest_post_file(self):
-        file_path = Constants.feeds_data_dir + '/' + self.feed_config['name']
+    def _register_latest_post(self, news_to_save):
+        if news_to_save != []:
+            self.latest_post = news_to_save[0].title
+            file_path = Constants.feeds_data_dir + '/' + self.feed_config['name']
+            with open(file_path, 'w') as file_buff:
+                file_buff.write(self.latest_post)
 
-        with open(file_path, 'w') as file_buff:
-            file_buff.write(self.latest_post)
-
-    def _update_latest_post_data(self, news_content):
-        self.latest_post = news_content
-
-    def _close_thread_after_usage(self) -> None:
+    def _close_thread(self) -> None:
         sys.exit()
 
-    def _get_xml_feed(self, feed_url, generator_exist):
-        xml_entries = feedparser.parse(feed_url).entries
-        if xml_entries != []:
-            return xml_entries
-        elif generator_exist:
-            rss_feed = RSSGenerator(feed_url).render_xml_feed()
-            return feedparser.parse(rss_feed).entries
-        else:
-            raise Exception(f"URL: {feed_url} is not a valid url")
-
-    def _send_message_if_needed(self, case) -> None:
-        no_need_to_post, post_until_latest, add_all_post_until_time = (0, 1, 2)
-
-        if no_need_to_post == case:
-            self._nothing_to_post(f"There is no new post on {self.feed_config['name']}")
-        elif post_until_latest == case:
-            self._post_until_latest()
-        elif add_all_post_until_time == case:
-            self._append_new_feed()
-
-    def _nothing_to_post(self, message) -> None:
-        logger.info(message)
-
-    def _post_until_latest(self) -> None:
-        news_to_publish = self._get_unsended_news()
-
-        for i, new_post in enumerate(reversed(news_to_publish)):
-            self.message.send_message(new_post)
-
-            is_last_news = i == len(news_to_publish) - 1
-            if is_last_news:
-                self._update_latest_post_data(news_to_publish[0].title)
-        self._register_latest_post_file()
-
-    def _append_new_feed(self) -> None:
-        news_to_publish = self._get_unsended_news()
-
-        for i, new_post in enumerate(reversed(news_to_publish)):
-            self.message.send_message(new_post)
-            is_last_news = i == len(news_to_publish) - 1
-            if is_last_news:
-                self._update_latest_post_data(news_to_publish[0].title)
-        self._register_latest_post_file()
-
-    def _get_unsended_news(self):
-        news_to_publish = []
-        feed_already_registered = self.latest_post != ''
-
-        if feed_already_registered:
-            for single_news in self.all_posts:
-                if single_news.title != self.latest_post:
-                    news_to_publish.append(single_news)
-                elif single_news.title == self.latest_post or not self._is_too_old_news(single_news):
-                    break
-        else:
-            for single_news in self.all_posts:
-                if not self._is_too_old_news(single_news):
-                    news_to_publish.append(single_news)
-                else:
-                    break
-
-        if news_to_publish == []:
+    def _send_message_if_needed(self) -> None:
+        if self.news_to_publish == []:
             logger.info(f"{self.feed_config['name']}: No news to share")
+        else:
+           self._post_news_on_discord(self.news_to_publish)
 
-        return news_to_publish
+    def _post_news_on_discord(self, news_to_publish) -> None:
+        for new_post in news_to_publish:
+            self.message.send_message(new_post)
 
     def _is_too_old_news(self, single_news):
         date_time = parser.parse(single_news['published'])
@@ -112,18 +54,34 @@ class Feed:
         ) - date_time.astimezone(timezone)
         return not time_since_published.total_seconds() <= self.feed_config['published_since']
 
-    def _get_status_of_feed(self) -> int:
+    def _get_unsended_news(self, all_posts):
+        news_to_publish = []
+        news_to_save = news_to_publish
 
-        no_need_to_post, post_until_latest, add_all_post_until_time = (0, 1, 2)
-        feed_already_registered = self.latest_post != ''
-
-        if feed_already_registered:
-            current_feed_is_registered = self.all_posts[0].title == self.latest_post
-            if current_feed_is_registered:
-                return no_need_to_post
-            else:
-                return post_until_latest
-        elif int(self.feed_config['published_since']) == 0:
-            return no_need_to_post
+        if self.latest_post != '':
+            for single_news in all_posts:
+                if single_news.title == self.latest_post:
+                    break
+                news_to_publish.append(single_news)
         else:
-            return add_all_post_until_time
+            if int(self.feed_config['published_since']) == 0:
+                news_to_save = [all_posts[0]]
+            else:
+                for single_news in all_posts:
+                    if self._is_too_old_news(single_news):
+                        break
+                    news_to_publish.append(single_news)
+        self._register_latest_post(news_to_save)
+        reversed_list_from_oldest_to_earliest = list(reversed(news_to_publish))
+
+        return reversed_list_from_oldest_to_earliest
+
+    def _get_xml_feed(self, feed_url):
+        xml_entries = feedparser.parse(feed_url).entries
+        if xml_entries != []:
+            return xml_entries
+        elif self.generator_exist:
+            rss_feed = RSSGenerator(feed_url).render_xml_feed()
+            return feedparser.parse(rss_feed).entries
+        else:
+            raise Exception(f"URL: {feed_url} is not a valid url")
